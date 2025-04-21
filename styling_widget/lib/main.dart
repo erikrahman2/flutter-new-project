021cbc4419cab6b36a9cae59a449a56989437482
erikrahman2 <erikrahman241203@gmail.com>
2025-04-21 07:46:07 +0700

    add flutter project files

---
+++ styling_widget/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Theme Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(
+            fontSize: 18,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home with Theme'),
+      ),
+      body: Center(
+        child: Text(
+          'Ini teks dengan tema!',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      ),
+    );
+  }
+}
